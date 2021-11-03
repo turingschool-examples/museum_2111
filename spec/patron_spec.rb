f1b5557dc60c31b83f5e0645bf254affc9bda038
Jimmy Page <jpage@knights.ucf.edu>
2021-11-03 09:29:00 -0600

    completed tests for iteration 1

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -18,5 +18,19 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
 
     expect(patron_1.spending_money).to eq(20)
-  end 
+  end
+
+  it 'persons interests are initially an empty array' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.interests).to eq([])
+  end
+
+  it 'can add a persons interests and store them in an array' do 
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
 end
