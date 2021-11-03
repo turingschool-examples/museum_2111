45774a595ff573bd6414659ef47bb2adc79206b3
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-11-03 09:16:31 -0600

    Test: add test for interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Patron do
     it "has attributes" do
       expect(patron_1.name).to eq "Bob"
       expect(patron_1.spending_money).to eq 20
+      expect(patron_1.interests).to eq []
     end
   end
 end
