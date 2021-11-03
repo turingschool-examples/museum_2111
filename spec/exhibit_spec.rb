e0286c28b740e5b9b7c2d49ccc414d95597c4dcc
Katy <m.catherine.harrod@gmail.com>
2021-11-03 09:07:14 -0600

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,11 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe do
+
+  it 'exists' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_a(Exhibit)
+  end
+
+end
