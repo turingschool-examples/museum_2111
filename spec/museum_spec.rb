c8b0e8c35ae2b28cf9a801a0ce09e09f47977266
Sam Iavri <sammi80134@gmail.com>
2021-11-03 09:57:09 -0600

    museum has new exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -17,4 +17,13 @@ RSpec.describe Museum do
     dmns = Museum.new('Denver Museum of Nature and Science')
     expect(dmns.exhibits).to eq([])
   end
+
+  it 'has new exhibits' do
+    gems_and_minerals = Exhibit.new({ name: 'Gems and Minerals', cost: 0 })
+    dead_sea_scrolls = Exhibit.new({ name: 'Dead Sea Scrolls', cost: 10 })
+    imax = Exhibit.new({ name: 'IMAX', cost: 15 })
+    expect(gems_and_minerals).to be_an_instance_of(Exhibit)
+    expect(dead_sea_scrolls).to be_an_instance_of(Exhibit)
+    expect(imax).to be_an_instance_of(Exhibit)
+  end
 end
