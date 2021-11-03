771be219d9b073f0ffd1eb2132cefe76512a591f
David Kassin <david.j.kassin@gmail.com>
2021-11-03 09:29:00 -0600

    test: Show add_interest can add interest to interest array

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,11 +2,19 @@ require './lib/patron'
 
 describe Patron do
   it 'exists' do
-    patron = Patron.new("Bob", 20)
+    patron_1 = Patron.new("Bob", 20)
 
-    expect(patron).to be_a Patron
-    expect(patron.name).to eq ("Bob")
-    expect(patron.spending_money).to eq 20
-    expect(patron.interests).to eq ([])
+    expect(patron_1).to be_a Patron
+    expect(patron_1.name).to eq ("Bob")
+    expect(patron_1.spending_money).to eq 20
+    expect(patron_1.interests).to eq ([])
   end
-end
+
+  it 'shows add_interests adds interests to interests array' do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq (["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+ end
