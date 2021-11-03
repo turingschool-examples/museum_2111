74b46fffb5012a6a15d30115f85fb316224774f2
David Kassin <david.j.kassin@gmail.com>
2021-11-03 10:10:16 -0600

    feat: Add recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,14 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits.append(exhibit)
   end
+
+  def recommend_exhibits(patron)
+    recommend = []
+    @exhibits.each do |exhibit|
+      if patron.interests.include?(exhibit.name) == true
+        recommend.append(exhibit)
+      end
+    end
+    recommend
+  end
 end
