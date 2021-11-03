343371a4ae80d6312bca4a9b23b47be875603c29
Parker <parkerlockhart42@gmail.com>
2021-11-03 10:26:06 -0500

    Add add_interest test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -15,5 +15,19 @@ describe Patron do
   it 'has spending_money' do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.spending_money).to eq(20)
-  end 
+  end
+
+  it 'has interests' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to eq([])
+  end
+  
+  describe 'Methods' do
+    it '#add_interest' do
+      patron_1 = Patron.new("Bob", 20)
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
+  end
 end
