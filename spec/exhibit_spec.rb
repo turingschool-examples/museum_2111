2dea8a73ecf3257261da2c4581835a8e33a2a949
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-03 09:15:19 -0600

    Test: Create attributes testing

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,17 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it 'exists' do
+    expect(@exhibit).to be_instance_of(Exhibit)
+  end
+
+  it 'has attributes' do
+    expect(@exhibit.name).to be("Gems and Minerals")
+    expect(@exhibit.cost).to be(0)
+  end
+end
