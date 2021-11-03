1e77e102e0653e86c051ded74534042fcf0325c7
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:31:51 -0600

    Add #recommend_exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -34,4 +34,33 @@ RSpec.describe Museum do
 
     expect(@museum.exhibits).to eq(expected)
   end
+
+  it 'can #recommend_exhibits to an inputted patron' do
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @museum.add_exhibit(gems_and_minerals)
+    @museum.add_exhibit(dead_sea_scrolls)
+    @museum.add_exhibit(imax)
+
+    patron_1 = Patron.new("Sally", 20)
+    patron_2 = Patron.new("Bob", 20)
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2.add_interest("IMAX")
+
+    expected_one = [gems_and_minerals, dead_sea_scrolls]
+    expected_two = [imax]
+
+    expect(@museum.recommend_exhibits(patron_1)).to eq(expected_one)
+    expect(@museum.recommend_exhibits(patron_2)).to eq(expected_two)
+
+  end
+
+
+
+
+
 end
