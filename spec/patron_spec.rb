ebd7d7c46fae8fc4d61b0e81ab9bff3f7d961c89
David Kassin <david.j.kassin@gmail.com>
2021-11-03 09:23:56 -0600

    test: Show attr_reader methods work along with arguments

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,8 +2,11 @@ require './lib/patron'
 
 describe Patron do
   it 'exists' do
-    patron = Patron.new()
+    patron = Patron.new("Bob", 20)
 
     expect(patron).to be_a Patron
+    expect(patron.name).to eq ("Bob")
+    expect(patron.spending_money).to eq 20
+    expect(patron.interests).to eq ([])
   end
 end
