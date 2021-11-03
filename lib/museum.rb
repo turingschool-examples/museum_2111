6030ad7184f5624a9c4434acb566518ebbe94e9f
Joel Grant <joelmgrant@me.com>
2021-11-03 10:05:44 -0600

    Feat: Add recommend_exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,6 @@
 class Museum
-  attr_reader :name, :exhibits
-
+  attr_reader :name
+  attr_accessor :exhibits
   def initialize(name)
     @name = name
     @exhibits = []
@@ -9,5 +9,11 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits.push(exhibit)
   end
-  
+
+  def recommend_exhibits(patron)
+    recommendations = @exhibits.select do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
 end
