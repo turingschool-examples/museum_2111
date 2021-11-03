fc18c7ab7fad325886094170e88d1a65c4e68fd0
Croix Kimmel <croixkimmel@gmail.com>
2021-11-03 10:58:18 -0600

    Complete to ticket_lottery_contestants

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -25,4 +26,40 @@ class Museum
 
     recommended_exhibits
   end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    return_hash = {}
+
+    @exhibits.each do |exhibit|
+      patrons_for_exhibit = []
+
+      patrons.each do |patron|
+        if patron.interests.include?(exhibit.name)
+          patrons_for_exhibit << patron.name
+        end
+
+      return_hash[exhibit.name] = patrons_for_exhibit
+      end
+    end
+
+    return_hash
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    patrons_interested = []
+
+    patrons.each do |patron|
+      if patron.interests.include?(exhibit.name) && patron.spending_money < exhibit.cost
+        patrons_interested << patron
+      end
+    end
+
+    patrons_interested
+  end
+
+
 end
