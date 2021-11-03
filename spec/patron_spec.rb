f3ec12870606fc81df1a4b7cd3d1a8b42d7b9505
Joel Grant <joelmgrant@me.com>
2021-11-03 09:16:46 -0600

    Test: Add #initialize tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,5 +3,24 @@ require './lib/patron'
 
 describe Patron do
   before(:each) do
+    patron_1 = Patron.new("Bob", 20)
   end
-end 
+
+  describe '#initialize' do
+    it 'creates and instance of patron' do
+      expect(patron_1).to be_a Patron
+    end
+
+    it 'returns the patrons name' do
+      expect(patron_1.name).to eq("Bob")
+    end
+
+    it 'returns the patrons spending money' do
+      expect(patron_1.spending_money).to eq(20)
+    end
+
+    it 'returns the interests array instance variable' do
+      expect(patron_1.interests).to eq([])
+    end 
+  end
+end
