3f0a682cbb63e6ddc8d493545ec7142022d24fc2
Croix Kimmel <croixkimmel@gmail.com>
2021-11-03 09:31:23 -0600

    Add patron, exhibit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe 'Exhibit' do
     expect(exhibit.name).to eq ("Gems and Minerals")
   end
 
-
   it 'has a cost' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit.cost).to eq (0)
   end
+
 
 
 end
