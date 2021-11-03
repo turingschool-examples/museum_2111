f71688aa372852f2f85c979fc079e3242efdc1de
Joel Grant <joelmgrant@me.com>
2021-11-03 09:10:01 -0600

    Test: Add #initialize tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,22 @@ require './lib/exhibit'
 
 describe Exhibit do
   before(:each) do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
-end 
+
+  describe '#initialize' do
+    it 'creates an instance of Exhibit' do
+      expect(exhibit).to be_a Exhibit
+    end
+
+    it 'contains instance variable for name' do
+      expect(exhibit.name).to eq("Gems and Minerals")
+    end
+
+    it 'contains instance variable for cost' do
+      expect(exhibit.cost).to eq(0)
+    end
+  end
+
+
+end
