6b67f12f23cf492ae00bb8044de64fe5ca5cf06b
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-03 10:21:54 -0600

    Test: #patrons_by_exhibit_interest completed

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -66,9 +66,33 @@ RSpec.describe Museum do
       dmns.admit(patron_1)
       dmns.admit(patron_2)
       dmns.admit(patron_3)
-
+      
       expected = [patron_1, patron_2, patron_3]
       expect(dmns.patrons).to eq(expected)
     end
   end
+  
+  describe '#patrons_by_exhibit_interest' do
+    it 'can return hash of exhibits with values of interested patrons array' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3.add_interest('Dead Sea Scrolls')
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expected = {
+                  gems_and_minerals => [patron_1],
+                  dead_sea_scrolls => [patron_1, patron_2, patron_3],
+                  imax => []
+                  }
+      expect(dmns.patrons_by_exhibit_interest).to eq(expected)
+    end
+  end
+
+  
 end
