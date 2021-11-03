e2d3d5f0898917f3268350048c269e854e568ffe
Hannah Warren <hannahkwarren@gmail.com>
2021-11-03 12:34:30 -0400

    Add tests for returning patrons array and grouping patrons into a hash by interest

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe do
     @patron_1.add_interest("Gems and Minerals")
     @patron_2 = Patron.new("Sally", 20)
     @patron_2.add_interest("IMAX")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   it 'exists' do
@@ -35,6 +37,23 @@ RSpec.describe do
     @dmns.add_exhibit(@imax)
     expect(@dmns.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
     expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
+  end
+
+  it 'admits patrons' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expect(@dmns.patrons).to eq([])
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
+  end
+
+  it 'returns patrons by exhibit interest regardless how much money they have' do
+    expect(@dmns.patrons_by_exhibit_interest).to eq({@gems_and_minerals => [@patron_1],
+                                                    @dead_sea_scrolls => [@patron_1, @patron_3],
+                                                    @imax => [@patron_2]})
   end
 
 end
