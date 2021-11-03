f4fe0c1763666c46ac0d4a1cfcb10e4eb0ff830d
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-03 09:08:18 -0600

    Test: created initalize tests

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,20 @@
+require './lib/exhibit'
+
+RSpec.describe Exhibit do
+  let(exhibit) { Exhibit.new({name: 'Gems and Minerals', cost: 0}) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(exhibit).to be_a(Exhibit)
+    end
+
+    it 'has a name' do
+      expect(exhibit.name).to eq('Gems and Minerals')
+    end
+
+    it 'has a cost' do
+      expect(exhibit.cost).to eq(0)
+    end
+  end
+
+end
