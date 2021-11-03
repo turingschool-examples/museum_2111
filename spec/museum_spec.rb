2d5f4f8c32d1e5da4b48a845191bf2047e120669
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-03 10:12:29 -0600

    Test: Add testing for a method to sort patrons by their interests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -58,5 +58,28 @@ RSpec.describe Museum do
     @dmns.admit(@patron_2)
     @dmns.admit(@patron_3)
     expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
+  end
+
+  it 'can sort patrons by exhibit interest' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Dead Sea Scrolls")
+
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.patrons_by_exhibit_interest).to eq({@gems_and_minerals => [@patron_1],
+                                                    @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3]
+                                                    @imax => []})
   end 
 end
