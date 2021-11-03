4c26206723535b9fb40f9059199b9d5c969f2ec7
Hannah Warren <hannahkwarren@gmail.com>
2021-11-03 11:33:03 -0400

    Add basic tests for museum: exists and can add exhibits

---
+++ spec/museum_spec.rb
@@ -0,0 +1,29 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe do
+
+  before(:each) do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+  end
+
+  it 'exists' do
+    expect(@dmns).to be_instance_of(Museum)
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
+
+  it 'can add exhibits' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  
+
+end
