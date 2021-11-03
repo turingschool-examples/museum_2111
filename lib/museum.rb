c086f05bfe721fa6986db0730c6484347af95a63
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:45:51 -0600

    Add empty patrons array, attr_reader

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,10 @@
 class Museum
 
-  attr_reader :museum_name, :exhibits
+  attr_reader :museum_name, :exhibits, :patrons
   def initialize(museum_name)
     @museum_name = museum_name
     @exhibits = []
+    @patrons = []
   end
 
   # Push exhibits into exhibits[] array
@@ -25,7 +26,7 @@ class Museum
       end
     end
 
-    # Return recommended exhibits
+    # Return recommended exhibits (remove any nil values from mapping)
     patron_exhibits.compact
   end
 
