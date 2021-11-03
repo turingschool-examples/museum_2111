210fba7f3dfe3ca21803bf5da190a33d743758b2
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 09:44:15 -0600

    Complete Museum class

---
+++ lib/museum.rb
@@ -0,0 +1,22 @@
+require './lib/exhibit'
+
+class Museum
+
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+end
