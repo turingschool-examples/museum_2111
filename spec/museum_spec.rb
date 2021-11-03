bc9819b9bb6b7f0e5a2fcc9249e3b392f2f4c5c3
Paul Leonard <paulpaulleonard@gmail.com>
2021-11-03 11:48:49 -0400

    Museum complete. I2

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -30,11 +30,14 @@ RSpec.describe "Museum" do
   end
 
   it "can reccomend exhibits based on interests" do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
     @patron_2.add_interest("IMAX")
 
-    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
     expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
 end
