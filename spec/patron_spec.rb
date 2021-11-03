e19c1c9ceec3830f33a93eae4fda6e416c95f8c1
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-03 12:55:08 -0400

    Add tests for methods from the Patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,15 +3,35 @@ require './lib/patron'
 
 RSpec.describe Patron do
   it 'exists' do
-patron_1 = Patron.new("Bob", 20)
-expect(patron_1).to be_an_instance_of(Patron)
+    patron_1 = Patron.new('Bob', 20)
+    expect(patron_1).to be_an_instance_of(Patron)
   end
 
- it 'has a name' do
-   patron_1 = Patron.new("Bob", 20)
-   expect(patron_1.name).to eq("Bob")
+  it 'has a name' do
+    patron_1 = Patron.new('Bob', 20)
+    expect(patron_1.name).to eq('Bob')
+  end
 
-end
+  it 'has spending money' do
+    patron_1 = Patron.new('Bob', 20)
+    expect(patron_1.spending_money).to eq(20)
+  end
 
+  it 'has interests' do
+    patron_1 = Patron.new('Bob', 20)
+    expect(patron_1.interests).to eq([])
+  end
 
+  it 'adds interests' do
+    patron_1 = Patron.new('Bob', 20)
+
+    expect(patron_1.name).to eq('Bob')
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+
+    patron_1.add_interest('Dead Sea Scrolls')
+    patron_1.add_interest('Gems and Minerals')
+
+    expect(patron_1.interests).to eq(['Dead Sea Scrolls', 'Gems and Minerals'])
+  end
 end
