bc2680337d4051fe57f2816c687a054f18893eee
Joel Grant <joelmgrant@me.com>
2021-11-03 09:22:48 -0600

    Correct @ in test variables

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,22 +2,20 @@ require './lib/exhibit'
 
 describe Exhibit do
   before(:each) do
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
   describe '#initialize' do
     it 'creates an instance of Exhibit' do
-      expect(exhibit).to be_a Exhibit
+      expect(@exhibit).to be_a Exhibit
     end
 
     it 'contains instance variable for name' do
-      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(@exhibit.name).to eq("Gems and Minerals")
     end
 
     it 'contains instance variable for cost' do
-      expect(exhibit.cost).to eq(0)
+      expect(@exhibit.cost).to eq(0)
     end
   end
-
-
 end
