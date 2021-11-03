3dbcf198b11ee53abc188f8dffbbc527ff741dc0
Eldridge Turambi <eturambi95@gmail.com>
2021-11-03 09:18:19 -0600

    Add code for patron class that passes iteration 1 tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Patron do
   end
 
   it 'exsists' do
-    expect(@patron1).to be_an_instance_of(Patron)
+    expect(@patron_1).to be_an_instance_of(Patron)
   end
 
   it 'has attributes' do
-    expect(@patron1.name).to eq('Bob')
-    expect(@patron1.spending_money).to eq(20)
-    expect(@patron1.interests).to eq([])
+    expect(@patron_1.name).to eq('Bob')
+    expect(@patron_1.spending_money).to eq(20)
+    expect(@patron_1.interests).to eq([])
   end
 
   it '#add_interest' do
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
 
-    expect(@patron1.interests).to eq(['Dead Sea Scrolls', 'Gems and Minerals'])
+    expect(@patron_1.interests).to eq(['Dead Sea Scrolls', 'Gems and Minerals'])
   end
 end
