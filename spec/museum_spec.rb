1cac64f8e043df014bb92326b9341bf784fedd64
Brad Breiten <jonathan@breiten.us>
2021-11-03 09:30:17 -0600

    Add add_exhibit tests for museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,21 +4,33 @@ require './lib/exhibit'
 
 RSpec.describe Museum do
 
-let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+  let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
 
-describe '#initialize' do
-  it 'exists' do
-    expect(dmns).to be_an_instance_of(Museum)
+  let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+
+  let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
+
+  let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(dmns).to be_an_instance_of(Museum)
+    end
+
+    it 'has a name' do
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    end
+
+    it 'has exhibits' do
+      expect(dmns.exhibits).to eq([])
+    end
   end
 
-  it 'has a name' do
-    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  describe '#add_exhibit' do
+    it 'puts exhibit in exhibits array' do
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
   end
-
-  it 'has exhibits' do
-    expect(dmns.exhibits).to eq([])
-  end
-end
 
 
 end
