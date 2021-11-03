a9bcb28650cb46c7bc814ba62303be37f7cba760
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-03 11:20:04 -0400

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,19 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  it 'exists' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it 'has a name' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it 'has a cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.cost).to eq(0)
+  end
+end
