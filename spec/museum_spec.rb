d4f548e659c9fe96f2f5b4a749299684459bbd8c
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:24:23 -0600

    Add #add_exhibit method test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,4 +1,6 @@
 require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
 
 RSpec.describe Museum do
   before(:each) do
@@ -17,4 +19,19 @@ RSpec.describe Museum do
     expect(@museum.exhibits).to eq([])
   end
 
+
+
+  it 'can #add_exhibit into exhibits[]' do
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @museum.add_exhibit(gems_and_minerals)
+    @museum.add_exhibit(dead_sea_scrolls)
+    @museum.add_exhibit(imax)
+
+    expected = [gems_and_minerals, dead_sea_scrolls, imax]
+
+    expect(@museum.exhibits).to eq(expected)
+  end
 end
