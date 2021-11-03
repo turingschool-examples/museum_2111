e68fae4072c0de29cfe7bd40eb8119979358c89a
Brad Breiten <jonathan@breiten.us>
2021-11-03 09:14:35 -0600

    Add Patron initialize tests

---
+++ spec/patron_spec.rb
@@ -0,0 +1,23 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Patron do
+
+  let(:exhibit) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  let(:patron_1) {Patron.new("Bob", 20)}
+
+  describe '#intialize' do
+    it 'has a name' do
+      expect(patron_1.name).to eq "Bob"
+    end
+
+    it 'has spending money' do
+      expect(patron_1.spending_money).to eq 20
+    end
+
+    it 'has array of interests empty at start' do
+      expect(patron_1.interests).to eq([])
+    end
+  end
+
+end
