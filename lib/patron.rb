8c601ddb0ec9d284891b9bc2ccceb3f82bf411c8
Joel Grant <joelmgrant@me.com>
2021-11-03 09:27:35 -0600

    Feat: Add #add_interests

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,9 +1,13 @@
 class Patron
-  attr_accessor :name, :spending_money
+  attr_accessor :name, :spending_money, :interests
 
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
     @interests = []
+  end
+
+  def add_interest(interest)
+    @interests.push(interest)
   end
 end
