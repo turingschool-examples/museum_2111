63388276891d0109a5b747c92b001fa370ac9ca7
Joel Grant <joelmgrant@me.com>
2021-11-03 09:27:11 -0600

    Correct variables in patron tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,33 +3,33 @@ require './lib/patron'
 
 describe Patron do
   before(:each) do
-    patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 20)
   end
 
   describe '#initialize' do
     it 'creates and instance of patron' do
-      expect(patron_1).to be_a Patron
+      expect(@patron_1).to be_a Patron
     end
 
     it 'returns the patrons name' do
-      expect(patron_1.name).to eq("Bob")
+      expect(@patron_1.name).to eq("Bob")
     end
 
     it 'returns the patrons spending money' do
-      expect(patron_1.spending_money).to eq(20)
+      expect(@patron_1.spending_money).to eq(20)
     end
 
     it 'returns the interests array instance variable' do
-      expect(patron_1.interests).to eq([])
+      expect(@patron_1.interests).to eq([])
     end
   end
 
-  describe '#add_interests & #interests (retest)' do
+  describe '#add_interest & #interests (retest)' do
     it 'adds interests to the patrons list of interests and interests returns them' do
-      patron_1.add_interest("Dead Sea Scrolls")
-      patron_1.add_interest("Gems and Minerals")
-      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
     end
-  end 
+  end
 
 end
