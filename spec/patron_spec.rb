bdb1565cf2e1c762cb3ca144fc34ce7386d8d12d
Robin Dittrich <robinodittrich@gmail.com>
2021-11-03 08:07:17 -0700

    Create test files.

---
+++ spec/patron_spec.rb
@@ -0,0 +1,10 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  it 'exists' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exibit).to be_instance_of(Exhibit)
+  end
+end
