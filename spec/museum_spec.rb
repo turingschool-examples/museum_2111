675b26a54b3fbf0513ab5ecda022e77b8e73f9eb
Katy <m.catherine.harrod@gmail.com>
2021-11-03 09:36:50 -0600

    Add exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe Museum do
   it 'retrieves name' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
-  end 
+  end
+
+  it 'has no exhibits by default' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq([])
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  
 
 end
