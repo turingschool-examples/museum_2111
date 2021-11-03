a391284830f4a5440738c5f0bd8d8ce9012e8116
Chris Hewitt <agop5134@gmail.com>
2021-11-03 09:07:08 -0600

    Exhibit spec

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,17 @@
+require "./lib/exhibit"
+
+RSpec.describe Exhibit do
+
+  it "exists" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_an(Exhibit)
+  end
+
+  it "attributes" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end 
+
+
+end
