887e5b69213a12370fd8b404930bb43b827d036c
Eric Chon <echon92@gmail.com>
2021-11-03 09:20:25 -0600

    Created test to check for intrests after adding some

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -10,8 +10,14 @@ RSpec.describe Patron do
   end
 
   it "checks the attributes" do
-    expect(@patron_1.name).to be_an_instance_of("Bob")
-    expect(@patron_1.spending_money).to be_an_instance_of(20)
-    expect(@patron_1.interests).to be_an_instance_of([])
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
+    expect(@patron_1.interests).to eq([])
+  end
+
+  it "checks intrest after adding some interests " do
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 end
