699db323522785e781a53aed27f831d967c33d48
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:17:49 -0600

    Add exhibit_name/exhibit_cost attr_readers

---
+++ lib/exhibit.rb
@@ -0,0 +1,8 @@
+class Exhibit
+
+  attr_reader :exhibit_name, :exhibit_cost
+  def initialize(exhibit_info)
+    @exhibit_name = exhibit_info[:name]
+    @exhibit_cost = exhibit_info[:cost]
+  end
+end
