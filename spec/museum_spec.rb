61f9e17869fb49d5fa77c2201f4be9741bf32d3f
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-03 10:47:09 -0600

    Test: #draw_lottery_winner

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -84,15 +84,65 @@ RSpec.describe Museum do
       dmns.admit(patron_1)
       dmns.admit(patron_2)
       dmns.admit(patron_3)
-
+      
       expected = {
-                  gems_and_minerals => [patron_1],
-                  dead_sea_scrolls => [patron_1, patron_2, patron_3],
-                  imax => []
-                  }
+        gems_and_minerals => [patron_1],
+        dead_sea_scrolls => [patron_1, patron_2, patron_3],
+        imax => []
+      }
       expect(dmns.patrons_by_exhibit_interest).to eq(expected)
     end
   end
-
   
+  describe '#ticket_lottery_contestants' do
+    it 'can create list of contestants that cannot afford exhibit' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3.add_interest('Dead Sea Scrolls')
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+      
+      expected = [patron_1, patron_3]
+      expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq(expected)
+    end
+  end
+  
+  describe '#draw_lottery_winner' do
+    it 'can return a random lottery winner' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3.add_interest('Dead Sea Scrolls')
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+      allow(dmns).to receive(:draw_lottery_winner).with(dead_sea_scrolls).and_return(patron_1.name)
+
+      expect(dmns.draw_lottery_winner(dead_sea_scrolls)).to eq(patron_1.name)
+    end
+
+    it 'can return nil if no lottery contestants' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3.add_interest('Dead Sea Scrolls')
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.draw_lottery_winner(gems_and_minerals)).to eq(nil)
+    end
+  end
+
 end
