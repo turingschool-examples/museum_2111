3a9ecadf5a181c8e053989c3c26a7333eec63aea
John Napier <jcee.napier@gmail.com>
2021-11-03 09:54:19 -0600

    Adds #patrons_by_exhibit_interest method/addpatron

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -37,4 +37,40 @@ describe Museum do
             expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
         end
     end
+
+    describe 'iteration 3' do 
+        let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+        let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}  
+        let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}  
+        let(:imax) {Exhibit.new({name: "IMAX", cost: 15})}
+        let(:patron_1) {Patron.new("Bob", 20)}
+        let(:patron_2) {Patron.new("Sally", 20)}
+        let(:patron_3) {Patron.new("Johnny", 5)}
+
+        before(:each) do 
+            dmns.add_exhibit(gems_and_minerals)
+            dmns.add_exhibit(dead_sea_scrolls)
+            dmns.add_exhibit(imax)
+            patron_1.add_interest("Gems and Minerals")
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_2.add_interest("Dead Sea Scrolls")
+            patron_3.add_interest("Dead Sea Scrolls")
+            dmns.admit(patron_1)
+            dmns.admit(patron_2)
+            dmns.admit(patron_3)
+        end 
+
+        it 'can add patrons' do 
+            expect(dmns.patrons).to eq[patron_1, patron_2, patron_3]
+        end
+
+        it '#patrons_by_exhibit_interest' do 
+            expected = {
+            gems_and_minerals => [patron_1],
+            dead_sea_scrolls => [patron_1, patron_2, patron_3],
+            imax => []
+            }
+            expect(dmns.patrons_by_exhibit_interest).to eq(expected)
+        end
+    end
 end
