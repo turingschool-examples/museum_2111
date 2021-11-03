08b2b281ba4ee4ed25905bac60d501f523a55900
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-11-03 09:11:20 -0600

    Initial commit, Test: initialize

---
+++ spec/patron_spec.rb
@@ -0,0 +1,16 @@
+require './lib/patron'
+
+RSPec.deescribe Patron do
+  let!(patron_1){Patron.new("Bob", 20)}
+
+  describe '#initialize' do
+    it "exists" do
+      expect(patron_1).to be_a Patron
+    end
+
+    it "has attributes" do
+      expect(patron_1.name).to eq "Bob"
+      expect(patron_1.spending_money).to eq 20
+    end
+  end
+end
