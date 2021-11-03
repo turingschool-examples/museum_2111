038fde0a4820bfc977259cb3e59cef7770d72faa
Eric Chon <echon92@gmail.com>
2021-11-03 09:25:21 -0600

    created exist and attribute test for museum

---
+++ spec/museum_spec.rb
@@ -0,0 +1,19 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+
+RSpec.describe Museum do
+  before :each do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  it "exists" do
+    expect(@dmns).to be_an_instance_of(Museum)
+  end
+
+  it "checks the attributes" do
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
+end
