c94336ef4349aac76c1c0f6d244911463eebe68f
Eric Chon <echon92@gmail.com>
2021-11-03 09:11:23 -0600

    added test to check if exist and attributes

---
+++ spec/exibit_spec.rb
@@ -0,0 +1,16 @@
+require './lib/exhibit'
+
+RSpec.describe Exhibit do
+  before :each do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it "exists" do
+    expect(@exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it "checks the attributes" do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+    expect(@exhibit.cost).to eq(0)
+  end
+end
