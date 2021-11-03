41a4607d611b0a60bfa71ce16184a6866ad4445f
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-03 10:04:58 -0600

    Test: Create testing for admission of patrons method

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,12 +10,6 @@ RSpec.describe Museum do
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
 
-    @patron_1 = Patron.new("Bob", 20)
-    @patron_2 = Patron.new("Sally", 20)
-
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_2.add_interest("IMAX")
   end
 
   it 'exists' do
@@ -35,10 +29,34 @@ RSpec.describe Museum do
   end
 
   it 'can recommend exhibits to patrons based on their interests' do
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
     expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
     expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
   end
+
+  it 'can admit patrons' do
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Dead Sea Scrolls")
+
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
+  end 
 end
