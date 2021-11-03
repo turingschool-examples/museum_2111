a7504ebb353ce483243c003df49e751c502f0b6b
Sam Iavri <sammi80134@gmail.com>
2021-11-03 09:49:24 -0600

    Museum has exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe Exhibit do
     dmns = Museum.new('Denver Museum of Nature and Science')
     expect(dmns.name).to eq('Denver Museum of Nature and Science')
   end
+
+  it 'has a exhibits' do
+    dmns = Museum.new('Denver Museum of Nature and Science')
+    expect(dmns.exhibits).to eq([])
+  end
 end
