9492af4182487c5b553997901136f22428bfe637
Parker <parkerlockhart42@gmail.com>
2021-11-03 10:14:31 -0500

    Add attribute tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,7 +2,17 @@ require './lib/exhibit'
 
 describe Exhibit do
   it 'exists' do
-    exhibit = Exhibit.new
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit).to be_instance_of(Exhibit)
   end
-end 
+
+  it 'has a name' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it 'has a cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.cost).to eq(0)
+  end 
+end
