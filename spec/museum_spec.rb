cafbee7db8e445ce0f3b05f6f32c1c110d1ebda8
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-03 10:04:17 -0600

    Test: #admit completed

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Museum do
   let(:gems_and_minerals) { Exhibit.new({name: 'Gems and Minerals', cost: 0}) }
   let(:dead_sea_scrolls) { Exhibit.new({name: 'Dead Sea Scrolls', cost: 10}) }
   let(:imax) { Exhibit.new({name: 'IMAX', cost: 15}) }
-  let(:patron_1) { Patron.new('Bob', 20) }
+  let(:patron_1) { Patron.new('Bob', 0) }
   let(:patron_2) { Patron.new('Sally', 20) }
+  let(:patron_3) { Patron.new('Johnny', 5) }
   let(:dmns) { Museum.new('Denver Museum of Nature and Science') }
 
   describe '#initialize' do
@@ -21,6 +22,10 @@ RSpec.describe Museum do
 
     it 'has no exhibits initially' do
       expect(dmns.exhibits).to eq([])
+    end
+
+    it 'has no patrons initially' do
+      expect(dmns.patrons).to eq([])
     end
   end
 
@@ -43,9 +48,27 @@ RSpec.describe Museum do
       patron_1.add_interest('Dead Sea Scrolls')
       patron_1.add_interest('Gems and Minerals')
       patron_2.add_interest('IMAX')
-
+      
       expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
       expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+    end
+  end
+  
+  describe '#admit' do
+    it 'can admit patrons to the museum' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest('Gems and Minerals')
+      patron_1.add_interest('Dead Sea Scrolls')
+      patron_2.add_interest('Dead Sea Scrolls')
+      patron_3.add_interest('Dead Sea Scrolls')
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expected = [patron_1, patron_2, patron_3]
+      expect(dmns.patrons).to eq(expected)
     end
   end
 end
