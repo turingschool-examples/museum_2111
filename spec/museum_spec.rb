c5705833c519ef3779097726c14a8c97afc0ddec
Brad Breiten <jonathan@breiten.us>
2021-11-03 09:32:01 -0600

    Update Museum class add_exhibit tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -28,6 +28,12 @@ RSpec.describe Museum do
 
   describe '#add_exhibit' do
     it 'puts exhibit in exhibits array' do
+      dmns.add_exhibit(gems_and_minerals)
+
+      dmns.add_exhibit(dead_sea_scrolls)
+
+      dmns.add_exhibit(imax)
+
       expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
     end
   end
