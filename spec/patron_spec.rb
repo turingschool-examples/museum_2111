5b88811636190b212757ce7c7ce3a182eb06bb66
Katy <m.catherine.harrod@gmail.com>
2021-11-03 09:24:36 -0600

    Pass patron with no interests test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe do
   it 'retrieves name' do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.name).to eq("Bob")
-  end 
+  end
+
+  it 'retrieves spending money' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  it 'has no interests by default' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to eq([])
+  end
 
 end
