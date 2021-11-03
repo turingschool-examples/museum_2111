d2d2e3b8b3b87b279cde77b776e64f353c9996d1
Kelly Anderson <kean9528@colorado.edu>
2021-11-03 11:01:18 -0600

    more tests for iteration 3

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -37,6 +37,7 @@ describe Museum do
     expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
+
   it 'can admit patrons' do
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
@@ -53,5 +54,40 @@ describe Museum do
     dmns.admit(patron_2)
     dmns.admit(patron_3)
     expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+  end
+
+  it 'can sort patrons by interest' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("Dead sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons_by_exhibit_interest).to eq({dead_sea_scrolls => [patron_1, patron_2, patron_3], gems_and_minerals => [patron_1], imax => []})
+  end
+
+  it '#ticket_lottery_contestants' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("Dead sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
   end 
+
 end
