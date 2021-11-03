0e537834ead6ff90033ceedfe7b05e09f1f9f23e
Sam Iavri <sammi80134@gmail.com>
2021-11-03 10:15:12 -0600

    Museum has new exhibits added to the floor

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,13 @@
+require './lib/exhibit'
 class Museum
   attr_reader :name, :exhibits
 
   def initialize(name)
     @name = name
     @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
   end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -26,4 +26,16 @@ RSpec.describe Museum do
     expect(dead_sea_scrolls).to be_an_instance_of(Exhibit)
     expect(imax).to be_an_instance_of(Exhibit)
   end
+
+  it 'has the new exhibits add to the floor' do
+    dmns = Museum.new('Denver Museum of Nature and Science')
+    gems_and_minerals = Exhibit.new({ name: 'Gems and Minerals', cost: 0 })
+    dead_sea_scrolls = Exhibit.new({ name: 'Dead Sea Scrolls', cost: 10 })
+    imax = Exhibit.new({ name: 'IMAX', cost: 15 })
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
 end
