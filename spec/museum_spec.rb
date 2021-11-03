f65b1560a1b46f803bf01e66adbffbd7fcbff4c0
Joel Grant <joelmgrant@me.com>
2021-11-03 09:38:24 -0600

    Test: Add #add_exhibits and #exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,6 +5,9 @@ require './lib/museum'
 describe Museum do
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   describe '#initialize' do
@@ -20,4 +23,23 @@ describe Museum do
       expect(@dmns.exhibits).to eq([])
     end
   end
-end 
+
+  describe '#add_exhibits' do
+    it 'tests proper addition of exhibits' do
+      expect(@dmns.exhibits).to eq([])
+      @dmns.add_exhibit(@gems_and_minerals)
+      expect(@dmns.exhibits).to eq([@gems_and_minerals])
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls])
+      @dmns.add_exhibit(@imax)
+      expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+    end
+  end
+  describe '#exhibits' do
+    it 'returns the exhibits added at the museum' do
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
+      expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+    end
+end
