bfec2f18bfef1c9a575da028b9f490c54856db1f
David Kassin <david.j.kassin@gmail.com>
2021-11-03 09:24:20 -0600

    feat: Add attr_readers

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,4 +1,12 @@
 class Patron
-  def initialize
-  end 
+
+  attr_reader :name,
+              :spending_money,
+              :interests
+
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
 end
