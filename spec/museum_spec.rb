5ac453f0db26f01cad07a99bbaa34b7136213984
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-03 10:38:54 -0600

    Write tests for lottery method :ticket_lottery_contestants

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -89,5 +89,38 @@ describe Museum do
         expect(@museum.patrons_by_exhibit_interest).to eq({@dead_sea_scrolls => [@patron_1, @patron_3, @patron_2], @gems_and_minerals => [@patron_1], @imax => [] })
       end
     end
+
+    describe 'lottery methods' do
+      before(:each) do
+        @patron_1 = Patron.new("Bob", 0)
+        @patron_1.add_interest("Dead Sea Scrolls")
+        @patron_1.add_interest("Gems and Minerals")
+        @patron_2 = Patron.new("Sally", 20)
+        @patron_2.add_interest("Dead Sea Scrolls")
+        @patron_3 = Patron.new("Johnny", 5)
+        @patron_3.add_interest("Dead Sea Scrolls")
+        @museum.add_exhibit(@gems_and_minerals)
+        @museum.add_exhibit(@dead_sea_scrolls)
+        @museum.add_exhibit(@imax)
+        @museum.admit(@patron_1)
+        @museum.admit(@patron_2)
+        @museum.admit(@patron_3)
+      end
+      describe ' #ticket_lottery_contestants' do
+        it 'returns nil if no Patrons are eligible' do
+          expect(@museum.ticket_lottery_contestants(@dead_sea_scrolls)).to eq(nil)
+        end
+        it 'returns an array of Patrons' do
+          expect(@museum.ticket_lottery_contestants(@gems_and_minerals)).to be_a(Array)
+        end
+        it 'returns an array of Patrons if some patrons are eligible' do
+          expect(@museum.ticket_lottery_contestants(@imax).all?{|value| value.class == Patron}).to eq(true)
+        end
+        it 'returns the correct array of Patrons' do
+          expect(@museum.ticket_lottery_contestants(@gems_and_minerals)).to eq([@patron_1, @patron_3])
+          expect(@museum.ticket_lottery_contestants(@imax)).to eq([@patron_1, @patron_3])
+        end
+      end
+    end
   end
 end
