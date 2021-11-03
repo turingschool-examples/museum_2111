1d89366da0729188e8ebb20cafc8b807393b9f41
Kelly Anderson <kean9528@colorado.edu>
2021-11-03 10:52:19 -0600

    create test and method for add patron

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,33 +1,22 @@
 class Museum
-  attr_reader :name, :exhibits
-  def initialize(name, exhibits = [])
+  attr_reader :name, :exhibits, :patrons
+  def initialize(name, exhibits = [], patrons = [])
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
 
+  def admit(patron)
+    @patrons << patron
+  end
+
   def recommend_exhibits(patron)
-    # recommended_exhibits = []
-    # exhibits_interested = patron.interests.find_all do |interest|
-    #   exhibit_names
-    # end
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name) == true
     end
   end
-
-
-  # def recommend_exhibits(patron)
-  #   recommended_exhibits = []
-  #   exhibit_names = @exhibits.map do |exhibit|
-  #     exhibit[:name]
-  #   end
-  #   patron.interests.find do |interest|
-  #     interest == exhibit_names
-  #   end
-  #   recommended_exhibits
-  # end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,8 +8,7 @@ describe Museum do
   let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
   let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
   let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
-  let(:patron_1) {Patron.new("Bob", 20)}
-  let(:patron_2) {Patron.new("Sally", 20)}
+
   it 'exists' do
     expect(dmns).to be_an_instance_of(Museum)
   end
@@ -30,10 +29,29 @@ describe Museum do
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
     patron_2.add_interest("IMAX")
     expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
+  it 'can admit patrons' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.patrons).to eq([])
+    patron_1 = Patron.new("Bob", 20)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("Dead sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+  end 
 end
