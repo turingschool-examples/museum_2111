35a66233fa2a1af73022c72b0984add8bc7448ef
Jacob Jenner <jennerjacob@gmail.com>
2021-11-03 09:28:39 -0600

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,15 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it 'has a name' do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it 'has a cost' do
+    expect(@exhibit.cost).to eq(0)
+end
