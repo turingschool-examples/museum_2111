2c7e1cdb17631b9301298ac8bf2beb38e31d009f
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:42:30 -0600

    Create #recommend_exhibits method, add comments

--- lib/museum.rb
+++ lib/museum.rb
@@ -6,9 +6,27 @@ class Museum
     @exhibits = []
   end
 
+  # Push exhibits into exhibits[] array
   def add_exhibit(exhibit_object)
     @exhibits << exhibit_object
   end
 
+  def recommend_exhibits(patron_object)
+    patron_exhibits = []
+
+    # Look through museum exhibits and find matches with all patron interests
+    # Map all matches and push into patron_exhibits[]
+    @exhibits.map do |exhibit|
+      patron_object.interests.map do |interest|
+        # require 'pry'; binding.pry
+        if exhibit.exhibit_name == interest
+          patron_exhibits << exhibit
+        end
+      end
+    end
+
+    # Return recommended exhibits
+    patron_exhibits.compact
+  end
 
 end
