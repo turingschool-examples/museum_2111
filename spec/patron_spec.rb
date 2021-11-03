4a6f6f920c40685e80dbdbd3c5651126a258666c
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 09:08:25 -0600

    Add patron name/money read tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Patron do
   it 'exists' do
     patron = Patron.new("Bob", 20)
   end
+
+  it 'can read patron name' do
+    patron = Patron.new("Bob", 20)
+    expect(patron.name).to eq("Bob")
+  end
+
+  it 'can read patron money' do
+    patron = Patron.new("Bob", 20)
+    expect(patron.money).to eq(20)
+  end
 end
