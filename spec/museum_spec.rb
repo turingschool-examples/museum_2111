c648c4d6ef4fe7a42801369789c0199fc5efe1c9
David Kassin <david.j.kassin@gmail.com>
2021-11-03 09:42:43 -0600

    test: Show can add exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,11 +4,23 @@ require './lib/museum'
 
 describe Museum do
   it 'exists' do
-    museum = Museum.new("Denver Museum of Nature and Science")
+    dmns = Museum.new("Denver Museum of Nature and Science")
 
-    expect(museum).to be_a Museum
-    expect(museum.name).to eq ("Denver Museum of Nature and Science")
-    expect(museum.exhibits).to eq ([])
+    expect(dmns).to be_a Museum
+    expect(dmns.name).to eq ("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq ([])
 
+  end
+
+  it 'shows it can add exhitbits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits.count).to eq 3
   end
 end
