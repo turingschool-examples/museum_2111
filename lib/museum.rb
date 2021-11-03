67f88894074e643a795dfe2421e78427542999ba
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-03 10:27:40 -0600

    Update museum spec and add :admit and :patrons_by_exhibit methods.

--- lib/museum.rb
+++ lib/museum.rb
@@ -2,11 +2,12 @@ require './lib/patron'
 require './lib/exhibit'
 
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -17,5 +18,19 @@ class Museum
   def recommend_exhibits(patron)
     exhibits_of_interest = @exhibits.find_all{|exhibit| patron.interests.include?(exhibit.name)}
     exhibits_of_interest.sort_by{|exhibit| exhibit.name}
+  end
+
+  def admit(patron)
+    @patrons << patron
+    @patrons = @patrons.sort_by{|patron| patron.name}
+  end
+
+  def patrons_by_exhibit_interest
+    patrons_by_exhibit_interest = Hash.new()
+    @exhibits.each do |exhibit|
+      interested_patrons = @patrons.find_all{|patron| patron.interests.include?(exhibit.name)}
+      patrons_by_exhibit_interest[exhibit] = interested_patrons
+    end
+    return patrons_by_exhibit_interest
   end
 end
