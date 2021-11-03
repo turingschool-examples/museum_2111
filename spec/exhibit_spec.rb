003a5d1304c93e16a7ef5bebf17a194b472cc03c
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-03 09:12:02 -0600

    Write exhibit spec.

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,18 @@
+require './lib/patron'
+require './lib/exhibit'
+
+describe Exhbit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+  describe 'initialize' do
+    it 'exists' do
+      expect(@exhibit).to be_a(Exhibit)
+    end
+
+    it 'has_attributes' do
+      expect(@exhibit.name).to eq("Gems and Minerals")
+      expect(@exhibit.cost).to eq(0)
+    end
+  end
+end
