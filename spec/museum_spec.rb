4fc1cdb5668408541f25e7d796843893ee44fd9d
Parker <parkerlockhart42@gmail.com>
2021-11-03 10:39:40 -0500

    Add add_exhibit test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -16,5 +16,18 @@ describe Museum do
   it 'has exhibits' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
+  end
+
+  describe 'Methods' do
+    it '#add_exhibit' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
   end 
 end
