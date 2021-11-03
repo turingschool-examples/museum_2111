e45d571bb02c544ea9c9dc2464a34dc9c22f9ed4
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-03 09:55:30 -0600

    Feat: Creat reccomend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -7,5 +7,17 @@ class Museum
 
   def add_exhibit(exhibit_name)
     @exhibits.push(exhibit_name)
-  end 
+  end
+
+  def recommend_exhibits(patron)
+    @recommendations = []
+    patron.interests.each do |interest|
+      self.exhibits.each do |exhibit|
+        if exhibit.name == interest
+          @recommendations.push(exhibit)
+        end
+      end
+    end
+    @recommendations
+  end
 end
