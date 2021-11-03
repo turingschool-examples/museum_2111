917fad547efba78c68cb944e7234b7bca239cc54
Parker <parkerlockhart42@gmail.com>
2021-11-03 10:20:43 -0500

    Add attribute tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,7 +3,17 @@ require './lib/patron'
 
 describe Patron do
   it 'exists' do
-    patron = Patron.new
-    expect(patron).to be_instance_of(Patron)
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1).to be_instance_of(Patron)
   end
-end 
+
+  it 'has a name' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq("Bob")
+  end
+
+  it 'has spending_money' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.spending_money).to eq(20)
+  end 
+end
