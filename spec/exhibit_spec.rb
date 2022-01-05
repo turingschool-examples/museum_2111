0a67f4a928e2821c036da75964a72aa070cdd960
Jacob Jenner <jennerjacob@gmail.com>
2022-01-05 09:18:16 -0700

    Initial commit, files created and starter test written for Exhibit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,15 +1,14 @@
 require './lib/exhibit'
-require './lib/patron'
 
 RSpec.describe Exhibit do
   before(:each) do
-    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
-  it 'has a name' do
-    expect(@exhibit.name).to eq("Gems and Minerals")
+  it 'exists' do
+    expect(@exhibit).to be_a(Exhibit)
   end
 
-  it 'has a cost' do
-    expect(@exhibit.cost).to eq(0)
+
+
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,18 +1,1 @@
-require './lib/exhibit'
-require './lib/patron'
 
-RSpec.describe Patron do
-  before(:each) do
-    @patron_1 = Patron.new("Bob", 20)
-  end
-
-  it 'Has a name' do
-    expect(@patron_1.name).to eq("Bob")
-  end
-
-  it 'Can spend money' do
-    expect(@patron_1.spedning_money).to eq(20)
-  end
-
-  
-end
