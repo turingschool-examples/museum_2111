18362355018d49ef6e7ed9f78c91c5c538c941aa
Austin Moore <austinchristianmoore@gmail.com>
2022-01-05 11:37:57 -0500

    Feature: Create Museum tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -2,6 +2,51 @@ require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
 
-
 RSpec.describe Museum do
+  before (:each) do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  it "exists" do
+    expect(@dmns).to be_instance_of(Museum)
+  end
+
+  it "has a name" do
+    @dmns.name
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+  end
+
+  it "has no exhibits to start" do
+    @dmns.exhibits
+    expect(@dmns.exhibits).to eq([])
+  end
+
+  xit "adds exhibits" do
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15}
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+    @dmns.exhibits
+
+    expect(@dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  xit "recommends exhibits" do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    @dmns.recommend_exhibits(patron_1)
+    @dmns.recommend_exhibits(patron_2)
+
+    expect(@dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+    expect(@dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
 end
