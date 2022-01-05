da4aaf0647f91e3ba18eebee77b058974e128c51
oscar santos <oscar303data@gmail.com>
2022-01-05 10:13:16 -0700

    Feat: it can find patrons that have an interest in certain exhibit is created as a helper method that will be used for the patrons by exhibit interest method

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Museum do
     expect(dmns.patrons).to eq([patron_1, patron_2,patron_3])
   end
 
-  it 'can group patrons by exhibit interest' do
+  xit 'can group patrons by exhibit interest' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
@@ -93,5 +93,29 @@ RSpec.describe Museum do
       dead_sea_scrolls => [patron_1, patron_2, patron_3],
       imax => []
       })
+    end
+
+    it 'can find patrons that have an interest in certain exhibits' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      # expect(dmns.patrons).to eq([])
+      patron_1 = Patron.new("Bob", 0)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2 = Patron.new("Sally", 20)
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3 = Patron.new("Johnny", 5)
+      patron_3.add_interest("Dead Sea Scrolls")
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.patrons_with_certain_interest(gems_and_minerals)).to eq([patron_1])
+      expect(dmns.patrons_with_certain_interest(imax)).to eq([])
     end
   end
