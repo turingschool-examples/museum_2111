2248d5cdaac54f49294f7ddd99cfaf6723abd5c5
benistta <92293363+benistta@users.noreply.github.com>
2022-01-05 09:36:51 -0700

    Feat: adding exhibits to empty array. museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -18,4 +18,15 @@ RSpec.describe Museum do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
   end
+
+  it 'has exhibits is an empty array' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
 end
