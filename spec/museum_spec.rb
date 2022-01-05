80c5c2580ab86b2200eb27dc39fdef01af38f109
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:26:27 -0500

    Test: create recommends exhibit tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe do
     expect(dmns.exhibits).to eq([])
   end
 
-  it 'has exhibits' do
+  it 'adds exhibits' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
@@ -29,5 +29,36 @@ RSpec.describe do
 
 
     expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  it 'recommends exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+  end
+
+  it 'recommends to a different patron' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
 end
