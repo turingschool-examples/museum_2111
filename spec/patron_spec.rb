fe0327de1a1c4e5ff49cb6903ff06721cd876273
Tyler Ross <tross020890@gmail.com>
2022-01-05 10:25:06 -0600

    Feat: add first patron tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Patron do
 
     expect(patron_1).to be_instance_of(Patron)
   end
+
+  it 'has name' do
+
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+  end
+
+  it 'has money' do
+
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  it 'has interests' do
+
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.interests).to be_instance_of(Array)
+  end
 end
