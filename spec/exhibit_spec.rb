18b750627e9e3eaa038a588a2a61ae2ddbb0f64c
Sam Smith <samlsmith424@gmail.com>
2022-01-05 08:35:42 -0800

    Test: initialize and #name, #cost

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,15 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  let(:exhibit) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+
+  it 'exists' do
+    expect(exhibit).to be_a(Exhibit)
+  end
+
+  it 'has attributes' do
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+end
