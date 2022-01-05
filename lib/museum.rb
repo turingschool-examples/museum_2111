12af6350030fc0a0b2a0c88d7aa59ab86725d14a
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 12:49:51 -0500

    Final commit: Refactored

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,6 +9,7 @@ class Museum
     @exhibits = []
     @patrons = []
     @patrons_by_interest = Hash.new {|hash, key| hash[key] = []}
+    @patrons_of_exhibits = Hash.new {|hash, key| hash[key] = []}
   end
 
   def add_exhibit(exhibit)
@@ -17,7 +18,7 @@ class Museum
 
   def recommend_exhibits(patron)
     patron_interests = @exhibits.select do |exhibit|
-        exhibit.name if patron.interests.include?(exhibit.name)
+      exhibit.name if patron.interests.include?(exhibit.name)
     end
     patron_interests
   end
@@ -50,22 +51,3 @@ class Museum
     pool.sample
   end
 end
-
-dmns = Museum.new("Denver Museum of Nature and Science")
-gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-imax = Exhibit.new({name: "IMAX",cost: 15})
-dmns.add_exhibit(gems_and_minerals)
-dmns.add_exhibit(dead_sea_scrolls)
-dmns.add_exhibit(imax)
-patron_1 = Patron.new("Bob", 0)
-patron_1.add_interest("Gems and Minerals")
-patron_1.add_interest("Dead Sea Scrolls")
-patron_2 = Patron.new("Sally", 20)
-patron_2.add_interest("Dead Sea Scrolls")
-patron_3 = Patron.new("Johnny", 5)
-patron_3.add_interest("Dead Sea Scrolls")
-dmns.admit(patron_1)
-dmns.admit(patron_2)
-dmns.admit(patron_3)
-p dmns.ticket_lottery_contestants(dead_sea_scrolls)
