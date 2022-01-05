3e53aa5e3b546ebff669fbfd67fae23981161da0
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:11:08 -0700

    Add tests for existence, name, and cost attributes'

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,3 +1,7 @@
 class Exhibit
-
-end 
+  attr_reader :name, :cost
+  def initialize(hash)
+    @name = hash[:name]
+    @cost = hash[:cost]
+  end
+end
--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,9 +1,17 @@
 require './lib/exhibit'
+require 'pry'
 
 RSpec.describe Exhibit do
   before(:each) do
     @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
-  it 'has a name attribute' do
-    expect(@exhibit.name).to be("Gems and Minerals")
+  it 'creates an instance of Exhibit' do
+    expect(@exhibit).to be_instance_of(Exhibit)
   end
+  it 'has a name attribute' do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+  end
+  it 'has a cost attribute' do
+    expect(@exhibit.cost).to eq(0)
+  end 
+end
