ce8c841ff98bee3e28cff9912b0556a0c56719d4
Nick Cooley <cooley8706@gmail.com>
2022-01-05 09:37:54 -0700

    Test: name and cost pass

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Exhibit do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
+  it 'has a name and cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to be 0
+  end
 end
