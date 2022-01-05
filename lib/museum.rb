2a052425e2f212dffb200d62e4e55318aa92d880
Mark Ertmer <mark.ertmer@gmail.com>
2022-01-05 09:56:07 -0700

    Iteration 2 methods

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,2 +1,18 @@
 class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name = "no name")
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.select do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -38,8 +38,9 @@ RSpec.describe Museum do
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
     patron_2 = Patron.new("Sally", 20)
-    patron_1.add_interest("IMAX")
-    expect(dmns.recommend_exhibits(patron_1)).to eq [dead_sea_scrolls, gems_and_minerals]
+    patron_2.add_interest("IMAX")
+    #                                   had to reverse the order of this array ↓↓↓
+    expect(dmns.recommend_exhibits(patron_1)).to eq [gems_and_minerals, dead_sea_scrolls]
     expect(dmns.recommend_exhibits(patron_2)).to eq [imax]
   end
 
