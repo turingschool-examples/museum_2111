d5f5590ba57bbb8f92470842ecbdd2a1637daec4
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:11:51 -0500

    Test: create it has a name test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,10 +3,13 @@ RSpec.describe do Patron
   it 'exists' do
     patron_1 = Patron.new("Bob", 20)
 
-    expect(patron_1).to be_a(Patrong)
+    expect(patron_1).to be_a(Patron)
   end
 
-  it '' do
+  it 'has a name' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq('Bob')
   end
 
   it '' do
