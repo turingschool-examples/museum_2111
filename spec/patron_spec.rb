969767a32ac73e1d1516866a30c4f1fd4831be22
Jimmy Page <jpage@knights.ucf.edu>
2022-01-05 09:37:43 -0700

    created all necessary tests for patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,3 +1,26 @@
 require 'rspec'
 require './lib/patron'
 require './lib/exhibit'
+
+describe Patron do
+  it 'has a name' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+  end
+
+  it 'has spending money' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  it 'starts with an empty array of interests but can add interests' do
+   patron_1 = Patron.new("Bob", 20)
+
+   expect(patron_1.interests).to eq([])
+   patron_1.add_interest("Dead Sea Scrolls")
+   patron_1.add_interest("Gems and Minerals")
+   expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+end
