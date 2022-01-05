4f0f32cd4b7b312372f9b79325a08d9311755b64
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-05 11:59:38 -0500

    Add #recommends exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -23,5 +23,16 @@ RSpec.describe Museum do
       subject.add_exhibit(@imax)
       expect(subject.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
     end
+
+    it "recommends exhibits based on patron interests" do
+      subject.add_exhibit(@gems_and_minerals)
+      subject.add_exhibit(@dead_sea_scrolls)
+      subject.add_exhibit(@imax)
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      @patron_2.add_interest("IMAX")
+      expect(subject.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
+      expect(subject.recommend_exhibits(@patron_2)).to eq([@imax])
+    end
   end
 end
