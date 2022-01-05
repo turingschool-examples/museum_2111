596d367d872325ce9593c49a77106d3a583b02cd
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:15:51 -0700

    Test: Existence of Patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,9 +1,12 @@
-require './lib/exhibit'
+require './lib/patron'
 require 'pry'
 
 RSpec.describe Patron do
   before(:each) do
     @patron_1 = Patron.new('Bob', 20)
+  end
+  it 'will create an instance of patron' do
+    expect(@patron_1).to be_instance_of(Patron)
   end
 
 end
