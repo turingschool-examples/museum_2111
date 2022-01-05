7651d44ee22939c07dd330caa5a7be4e7ffc65e0
benistta <92293363+benistta@users.noreply.github.com>
2022-01-05 11:06:27 -0700

    commiting changes

--- lib/museum.rb
+++ lib/museum.rb
@@ -22,4 +22,23 @@ class Museum
     @patrons << patron
   end
 
+  def have_interests(patron)
+    @epatrons.select do |patron|
+      patron.interests.include?(exhibit.name)
+  end
+
+  def patrons_by_exhibit_interest
+    result_hash = {}
+    @exhibits.each do |exhibit|
+      @patrons.select do |patron|
+        if result_hash[exhibit].nil?
+          result_hash[exhibit] = phave_interests(patron)
+          require "pry"; binding.pry
+     end
+    end
+    end
+    result_hash
+
+  end
+
 end
