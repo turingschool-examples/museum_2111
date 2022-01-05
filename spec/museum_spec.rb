a400a28a3804c3098462b41920bf4c5cee117e59
Jesse Ochs <jessescottochs@gmail.com>
2022-01-05 09:44:25 -0700

    Test: tests written for iteration 3, museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Museum do
   let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
   let(:patron_1) {Patron.new("Bob", 20)}
   let(:patron_2) {Patron.new("Sally", 20)}
+  let(:patron_3) {Patron.new("Johnny", 5)}
 
   it 'exists' do
     expect(dmns).to be_a(Museum)
@@ -38,6 +39,7 @@ RSpec.describe Museum do
   xit 'exists' do
     expect(patron_1).to be_a(Patron)
     expect(patron_2).to be_a(Patron)
+    expect(patron_3).to be_a(Patron)
   end
 
   xit 'can add interests' do
@@ -53,6 +55,75 @@ RSpec.describe Museum do
     patron_2.add_interest("IMAX")
     expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
+  xit 'can list' do
+    expect(dmns.patrons).to eq([])
+  end
+
+  xit 'can add exhibits to museum' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  xit 'can admit patrons' do
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+  end
+
+  xit 'can add exhibits to museum' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expected = {
+      gems_and_minerals => [patron_1],
+      dead_sea_scrolls => [patron_1, patron_2, patron_3],
+      imax => []
+    }
+    expect(dmns.patrons_by_exhibit_interest).to eq(expected)
+  end
+
+  xit 'can run a ticket lottery per exhibit' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_2, patron_3])
+  end
+
+  xit 'draw a lottery winner based on interests and if they can afford the exhibit' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq("Johnny" || "Bob")
+    expect(dmns.draw_lottery_winner(gems_and_minerals)).to eq(nil)
   end
 
 
