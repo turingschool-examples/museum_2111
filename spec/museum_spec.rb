efceb7526c82ab3c020612bcf12815e56dd1ea43
Nick Cooley <cooley8706@gmail.com>
2022-01-05 10:25:16 -0700

    Test: museum exists pass

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,5 +1,6 @@
 require './lib/exhibit'
 require './lib/patron'
+require './lib/museum'
 
 RSpec.describe Exhibit do
   it 'exists' do
@@ -18,10 +19,22 @@ RSpec.describe Exhibit do
     expect(patron_1).to be_instance_of(Patron)
   end
 
-  it 'patron has money and interests' do
+  xit 'patron has money and no interests' do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.name).to eq("Bob")
     expect(patron_1.spending_money).to be 20
-    expect(patron_1.interests).to eq([])
+    expect(patron_1.interests).to be_empty
+  end
+
+  xit 'patron adds interests' do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expect(patron_1.interests).to eq["Dead Sea Scrolls", "Gems and Minerals"]
+  end
+
+  it 'has a museum' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns).to be_instance_of(Museum)
   end
 end
