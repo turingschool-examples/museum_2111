9f9d5fbbe513161e2dea205ec5f73ae144c787b0
Mark Ertmer <mark.ertmer@gmail.com>
2022-01-05 09:23:59 -0700

    Add: Iteration 1 methods

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,2 +1,13 @@
 class Patron
+  attr_reader :name, :spending_money, :interests
+
+  def initialize(name = "Clyde", spending_money = 0)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Patron do
 
   it 'exists' do
   patron = Patron.new
-  expect patron.to be_instance_of Patron
+  expect(patron).to be_instance_of Patron
   end
 
   it 'has a name and money' do
