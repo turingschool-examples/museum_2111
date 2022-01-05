d360ebfe5e44af57e1acbff6994fd21b8e13a243
oscar santos <oscar303data@gmail.com>
2022-01-05 09:45:24 -0700

    Test: recommend_exhibits method is created and pass the functionality test

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,10 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
 end
