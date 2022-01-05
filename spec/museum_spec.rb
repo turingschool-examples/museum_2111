962de8270951086fb705a2f95a2aca19a0faeb1e
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:20:46 -0500

    Test: create it has a name test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe do
 
     expect(dmns).to be_a(Museum)
   end
+
+  it 'has a name' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
 end
