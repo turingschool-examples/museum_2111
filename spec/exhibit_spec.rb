83ed520277d6c328857b1a6c9566819a72787d29
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:06:37 -0500

    Test: create it has a cost test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -11,16 +11,10 @@ RSpec.describe do
 
     expect(exhibit.name).to eq('Gems and Minerals')
   end
-  it '' do
-  end
-  it '' do
-  end
-  it '' do
-  end
-  it '' do
-  end
-  it '' do
-  end
-  it '' do
+
+  it 'has a cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.cost).to be(0)
   end
 end
