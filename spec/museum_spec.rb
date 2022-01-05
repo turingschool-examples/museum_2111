1505acadd6523b5eb1a423835959cf43234e5b0c
Jesse Ochs <jessescottochs@gmail.com>
2022-01-05 09:27:52 -0700

    Test: tests written for iteration 2, museum class

---
+++ spec/museum_spec.rb
@@ -0,0 +1,59 @@
+require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Museum do
+  let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+  let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
+  let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
+  let(:patron_1) {Patron.new("Bob", 20)}
+  let(:patron_2) {Patron.new("Sally", 20)}
+
+  it 'exists' do
+    expect(dmns).to be_a(Museum)
+  end
+
+  xit 'has a name' do
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
+
+  xit 'can have exhibits' do
+    expect(dmns.exhibits).to eq([])
+  end
+
+  xit 'exists' do
+    expect(gems_and_minerals).to be_a(Exhibit)
+    expect(dead_sea_scrolls).to be_a(Exhibit)
+    expect(imax).to be_a(Exhibit)
+  end
+
+  xit 'can add exhibits' do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+  end
+
+  xit 'exists' do
+    expect(patron_1).to be_a(Patron)
+    expect(patron_2).to be_a(Patron)
+  end
+
+  xit 'can add interests' do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+
+  xit 'can reccomend exhibits based on interests' do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
+
+end
