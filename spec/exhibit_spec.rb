c1f87592f8601ab56e05cdf6f0404c01b9b5268f
Maximilian Wagner <maw3350@gmail.com>
2022-01-05 12:06:16 -0500

    test:add initial test for exhibit_spec

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,14 @@
+require './lib/patron'
+require 'rspec'
+
+RSpec.describe Patron do
+
+  let(:exhibit) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+
+  context 'iteration I' do
+
+    it 'exists' do
+      expect(exhibit).to be_a Exhibit
+    end
+  end
+end
