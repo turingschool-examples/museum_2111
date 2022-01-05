183dd3b44b1d2a7c77f319d72e1ad0e5ec2db866
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:11:06 -0500

    Test: create it exists test

---
+++ spec/patron_spec.rb
@@ -0,0 +1,29 @@
+require './lib/patron'
+RSpec.describe do Patron
+  it 'exists' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1).to be_a(Patrong)
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+
+  it '' do
+  end
+end
