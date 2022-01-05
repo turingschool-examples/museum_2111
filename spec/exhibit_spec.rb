b4e24278d05a83f683f6792b13914529b3e8d871
Mark Ertmer <mark.ertmer@gmail.com>
2022-01-05 09:18:05 -0700

    Add: Iteration 1 tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -4,4 +4,16 @@ require './lib/museum.rb'
 require 'pry'
 
 RSpec.describe Exhibit do
+
+  it 'exists' do
+    exhibit = Exhibit.new
+    expect(exhibit).to be_instance_of Exhibit
+  end
+
+  it 'has a name and a cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq "Gems and Minerals"
+    expect(exhibit.cost).to be 0
+  end
+
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -4,4 +4,24 @@ require './lib/museum.rb'
 require 'pry'
 
 RSpec.describe Patron do
+
+  it 'exists' do
+  patron = Patron.new
+  expect patron.to be_instance_of Patron
+  end
+
+  it 'has a name and money' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq "Bob"
+    expect(patron_1.spending_money).to be 20
+  end
+
+  it 'develops interests' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to eq []
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expect(patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
+  end
+
 end
