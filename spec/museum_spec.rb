e556549e206dda7cf8b88df3b3ad932f9e75288e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 10:22:10 -0600

    Test: Museum initializes with a name and no exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,8 +1,20 @@
 require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
 
 RSpec.describe Museum do
 
   it 'exists' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns).to be_instance_of Museum
+  end
+
+  it 'initializes with a name and no exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq([])
   end
 
 end
