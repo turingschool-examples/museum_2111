788184b3015956d84c38aa0bd18ea1b89ab0f056
Chris_K <ckjolhede@gmail.com>
2022-01-05 09:43:01 -0700

    test: #add_interest working, iteration 1 complete - passing

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,15 +3,17 @@ require './lib/patron'
 require 'pry'
 
 RSpec.describe Patron do
-  let(:patron_1) {Patron.new("Bob", 20)}
+  # let(:patron_1) {Patron.new("Bob", 20)}
 
-  it 'exists' do
-    expect(patron_1).to be_a(Patron)
-  end
+    it 'exists' do
+      patron_1 = Patron.new("Bob",20)
+      expect(patron_1).to be_a(Patron)
+    end
 
-  it '#add_interest receives elements' do
-    patron_1.add_interest("Gems and Minerals")
-    patron_1.add_interest("Dead Sea Scrolls")
-    expect(patron_1).to eq(["Gems and Minerals", "Dead Sea Scrolls"])
-  end
+    it '#add_interest receives elements' do
+      patron_1 = Patron.new("Bob",20)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      expect(patron_1.interests).to eq(["Gems and Minerals", "Dead Sea Scrolls"])
+    end
 end
