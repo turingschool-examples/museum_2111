0b93697b17b0d0fee2e1623cbe2cad8b50381a11
Austin Moore <austinchristianmoore@gmail.com>
2022-01-05 12:30:01 -0500

    Feature: Pass It2 tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -7,4 +7,19 @@ class Museum
     @exhibits = []
   end
 
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    #for each patron's interest (array)
+    #check if interest matches with exhibit's name (array of hashes)
+    #if true, hash needs to be added to array
+    #if false, next hash should be compared
+    @exhibits.select do |exhibit|
+      patron.interests.any? do |interest|
+        exhibit.name == interest
+      end
+    end
+  end
 end
