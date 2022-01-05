c6f2ab60d1656cb7b8f6a1211630d8c09ef3a917
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:57:36 -0700

    Test: #patrons_by_exhibit_interest

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -55,6 +55,23 @@ RSpec.describe Museum do
       @dmns.admit(@patron_3)
       expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
     end
-  end
+    it 'will create a hash with the exhibit and an array of interested patrons' do
+      @dmns.add_exhibit(@imax)
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      @patron_2.add_interest("Dead Sea Scrolls")
+      @patron_3.add_interest("Dead Sea Scrolls")
+      @dmns.admit(@patron_1)
+      @dmns.admit(@patron_2)
+      @dmns.admit(@patron_3)
+      expect(@dmns.patrons_by_exhibit_interest).to be({
+        @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+        @gems_and_minerals => [@patron_1],
+        @imax => []
+        })
+      end
+    end
 
 end
