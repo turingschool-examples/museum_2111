40373818368c0ebb0869c62538c13a0153e37e61
oscar santos <oscar303data@gmail.com>
2022-01-05 09:20:42 -0700

    Feat: it has attributes block is created

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Exhibit do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit).to be_a(Exhibit)
   end
+
+  it 'has attributes' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
 end
