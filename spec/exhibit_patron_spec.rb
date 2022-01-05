678931f2a077fc356c0d5bc8e9af03fc96910078
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-05 11:14:15 -0500

    Add #has a patron test

--- spec/exhibit_patron_spec.rb
+++ spec/exhibit_patron_spec.rb
@@ -3,9 +3,18 @@ require './lib/patron'
 
 RSpec.describe Exhibit do
   subject { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+  let(:patron_1) { Patron.new("Bob", 20) }
   context "iteration-1" do
     it "exists" do
-    expect(subject.name).to eq("Gems and Minerals")
-    expect(subject.cost).to eq(0)
+      expect(subject.name).to eq("Gems and Minerals")
+      expect(subject.cost).to eq(0)
+    end
   end
+
+  it "has a patron" do
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+  end
+
 end
