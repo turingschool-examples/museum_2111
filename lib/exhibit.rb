58c209af9f02bfee28d133a8566606fa4ec58c59
benistta <92293363+benistta@users.noreply.github.com>
2022-01-05 09:14:50 -0700

    Feat: Added method name

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,6 +1,6 @@
 class Exhibit
-
+attr_reader :name
   def initialize(attributes)
-  
+    @name = attributes[:name]
   end
 end
--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Exhibit do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit).to be_instance_of(Exhibit)
   end
+
+  it 'has name' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
 end
