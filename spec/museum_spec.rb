f69c3185dc958b4b33403f3d19220f9d07a33844
oscar santos <oscar303data@gmail.com>
2022-01-05 10:26:45 -0700

    Feat: it can find patrons that do not have enough money block is created

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Museum do
     expect(dmns.patrons).to eq([patron_1, patron_2,patron_3])
   end
 
-  xit 'can group patrons by exhibit interest' do
+  it 'can group patrons by exhibit interest' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
@@ -117,5 +117,28 @@ RSpec.describe Museum do
 
       expect(dmns.patrons_with_certain_interest(gems_and_minerals)).to eq([patron_1])
       expect(dmns.patrons_with_certain_interest(imax)).to eq([])
+    end
+
+    it 'can list patrons that do not have enough money to see an exhibit' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      # expect(dmns.patrons).to eq([])
+      patron_1 = Patron.new("Bob", 0)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2 = Patron.new("Sally", 20)
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3 = Patron.new("Johnny", 5)
+      patron_3.add_interest("Dead Sea Scrolls")
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to eq([patron_1, patron_3])
     end
   end
