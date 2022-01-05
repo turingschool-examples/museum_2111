7475a4cc2f018012f86b68e9949c89159eb4c22c
oscar santos <oscar303data@gmail.com>
2022-01-05 09:28:54 -0700

    Feat: it has attributes block is created

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Museum do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns).to be_a(Museum)
   end
+
+  it 'has attributes' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(dmns.exhibits).to eq([])
+  end
 end
