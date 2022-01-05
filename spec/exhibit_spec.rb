c2e433a449d83432b73fc159e6096bc937a571f6
Austin Moore <austinchristianmoore@gmail.com>
2022-01-05 11:55:20 -0500

    Fix: Revert before feature to attempt error fix

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,22 +2,21 @@ require './lib/exhibit'
 require './lib/patron'
 
 RSpec.describe Exhibit do
-  before (:each) do
-    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-  end
-
   it "exists" do
-    expect(@exhibit).to be_instance_of(Exhibit)
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_instance_of(Exhibit)
   end
 
   it "has a name" do
-    @exhibit.name
-    expect(@exhibit.name).to eq("Gems and Minerals")
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    exhibit.name
+    expect(exhibit.name).to eq("Gems and Minerals")
   end
 
   it "has a cost" do
-    @exhibit.cost
-    expect(@exhibit.cost).to eq(0)
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    exhibit.cost
+    expect(exhibit.cost).to eq(0)
   end
 
 end
