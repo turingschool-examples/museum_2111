2799efcea6713d4cf6f820c8cc24abcff4d7fac0
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 10:10:05 -0600

    Initialize Exhibit class with name and cost

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -3,6 +3,16 @@ require './lib/exhibit'
 RSpec.describe Exhibit do
 
   it 'exists' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit).to be_instance_of Exhibit
   end
 
-end 
+  it 'initializes with a name and cost' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+
+end
