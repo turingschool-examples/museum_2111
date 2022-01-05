e4a283196c17c2492e02e6f561fbe94f0f26fbe1
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-05 11:17:54 -0500

    feat: add patron interests and add interets tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -6,9 +6,29 @@ RSpec.describe Patron do
     expect(patron_1).to be_a(Patron)
   end
 
-  it 'has a name' do
+  xit 'has a name' do
     patron_1 = Patron.new("Bob", 20)
     # binding.pry
-    expect(patron_1.name).to be_a("Bob")
+    expect(patron_1.name).to eq("Bob")
+  end
+
+  xit 'has a spending money' do
+    patron_1 = Patron.new("Bob", 20)
+    # binding.pry
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  xit 'has no interests by default' do
+    patron_1 = Patron.new("Bob", 20)
+    # binding.pry
+    expect(patron_1.interests).to eq([])
+  end
+
+  xit 'can gain new interests' do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    # binding.pry
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 end
