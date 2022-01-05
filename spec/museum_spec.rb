1e94c2ebc2ec9e88ea8c5689736d301b7cc981e5
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-05 11:58:39 -0500

    feat: pass has a name test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Patron do
     expect(dmns).to be_a(Museum)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     # binding.pry
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
