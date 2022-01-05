ec87e02771744f87053fbb70cb4179763077f500
benistta <92293363+benistta@users.noreply.github.com>
2022-01-05 09:22:33 -0700

    Feat Added method spending money

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.name).to eq("Bob")
   end
+
+  it 'has spending money' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.spending_money).to eq(20)
+  end
 end
