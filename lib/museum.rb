0388088b5a5a9d636f3572e3d41ded1bcfa681bd
Sam Smith <samlsmith424@gmail.com>
2022-01-05 08:59:47 -0800

    Test: #add_exhibit and #recommend_exhibits

---
+++ lib/museum.rb
@@ -0,0 +1,25 @@
+class Museum
+  attr_reader :name,
+              :exhibits,
+              :patrons
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+    @patrons = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits.push(exhibit)
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def admit(patron)
+    @patrons.push(patron)
+  end
+end
