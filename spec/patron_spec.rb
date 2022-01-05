c00b204d3d8ba628783f8c58307016946aa24944
Chris_K <ckjolhede@gmail.com>
2022-01-05 09:31:39 -0700

    test: added #add_interest

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Patron do
   it 'exists' do
     expect(patron_1).to be_a(Patron)
   end
+
+  it '#add_interest receives elements' do
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    expect(patron_1).to eq(["Gems and Minerals", "Dead Sea Scrolls"])
+  end
 end
