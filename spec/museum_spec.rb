2dac08372866d8f2f6211255daac38b88dee4fff
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-05 12:05:53 -0500

    Add #admits patron test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Museum do
   before(:each) do
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
@@ -33,6 +34,15 @@ RSpec.describe Museum do
       @patron_2.add_interest("IMAX")
       expect(subject.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
       expect(subject.recommend_exhibits(@patron_2)).to eq([@imax])
+    end
+  end
+  context "iteration-2" do
+    it "admits patrons" do
+      expect(subject.patrons).to eq([])
+      subject.admit(@patron_1)
+      subject.admit(@patron_2)
+      subject.admit(@patron_3)
+      expect(subject.patrons).to eq([@patron_1,@patron_2,@patron_3])
     end
   end
 end
