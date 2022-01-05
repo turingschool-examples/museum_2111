d6ac7a3a23388f8faf36aa02c7edc1234de20ca7
Luke Pascale <lpluke5@gmail.com>
2022-01-05 10:01:18 -0700

    Function: Seperate different tests into different spec folders.

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -4,41 +4,21 @@ require './lib/patron'
 require 'rspec'
 
 RSpec.describe Exhibit do
-  describe 'iteration 1' do
     it 'creates an instance of exhibit' do
-        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-        expect(exhibit).to be_an_instance_of(Exhibit)
-      end
+      expect(exhibit).to be_an_instance_of(Exhibit)
+    end
 
-      it 'can have a name' do
-        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    it 'can have a name' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-        expect(exhibit.name).to eq("Gems and Minerals")
-      end
+      expect(exhibit.name).to eq("Gems and Minerals")
+    end
 
-      it 'can have a cost' do
-        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    it 'can have a cost' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-        expect(exhibit.cost).to be(0)
-      end
-
-      it 'can create patrons' do
-        patron_1 = Patron.new("Bob", 20)
-
-        expect(patron_1).to be_an_instance_of(Patron)
-      end
-
-      it 'can give patrons names' do
-        patron_1 = Patron.new("Bob", 20)
-
-        expect(patron_1.name).to eq("Bob")
-      end
-
-      it 'can let the patron spend money' do
-        patron_1 = Patron.new("Bob", 20)
-
-        expect(patron_1.spending_money).to be(20)
-      end
+      expect(exhibit.cost).to be(0)
     end
   end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,31 @@
+require 'pry'
+require './lib/exhibit'
+require './lib/patron'
+require 'rspec'
+
+
+RSpec.describe Patron do
+  it 'can create patrons' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1).to be_an_instance_of(Patron)
+  end
+
+  it 'can give patrons names' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+  end
+
+  it 'can let the patron spend money' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.spending_money).to be(20)
+  end
+
+  it 'can list a patrons interests' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.interests).to eq([])
+  end
+end 
