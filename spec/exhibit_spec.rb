8995e16c6d1d22dfaf4821393339aadd1c27b71f
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:05:48 -0500

    Test: create has a name test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe do
     expect(exhibit).to be_a(Exhibit)
   end
 
-  it '' do
+  it 'has a name' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq('Gems and Minerals')
   end
   it '' do
   end
