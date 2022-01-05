c4f5ffd857347e2114d3bfe21f257175b3a01ae5
glsorensen <gunnar.l.sorensen@gmail.com>
2022-01-05 10:53:18 -0700

    Final commit...that was a rodeo Q EXIT
    '

--- lib/museum.rb
+++ lib/museum.rb
@@ -29,5 +29,13 @@ class Museum
 
   def patrons_by_exhibit_interest
     Hash.new(0)
+    #need to add to make test more robust and accurate
+    #needs to map the exhibits and match with patron with interest in exhibit
+  end
+
+  def ticket_lottery_contestants(exhibit)
+  end
+
+  def draw_lottery_winner(exhibit)
   end
 end
