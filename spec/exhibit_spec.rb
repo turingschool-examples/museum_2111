206fc8931e54ed4ad2cd281b2099311eda4ed54b
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-05 11:23:44 -0500

    fix: clean up exhibit spec to fucntion

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,18 +1,21 @@
+require 'RSpec'
+require './lib/exhibit'
+require 'pry'
 RSpec.describe Exhibit do
   it 'exists' do
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0}))
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     # binding.pry
     expect(exhibit).to be_a(Exhibit)
   end
 
   xit 'has a name' do
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0}))
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     # binding.pry
     expect(exhibit.name).to eq("Gems and Minerals")
   end
 
   xit 'has a cost' do
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0}))
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     # binding.pry
     expect(exhibit.cost).to eq(0)
   end
