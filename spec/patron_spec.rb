7cdc804a31fe5487e7f11f3dc6c248a6bf096ed8
oscar santos <oscar303data@gmail.com>
2022-01-05 09:10:01 -0700

    it has attributes block is created

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1).to be_a(Patron)
   end
+
+  it 'has attrbutes' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
+  end
 end
