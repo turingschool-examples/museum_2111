6e506e28a4af508db861f0742e0a853a303ce0f0
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:49:53 -0700

    Testing: add testing for #admit

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Museum do
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
   end
   it 'will create an instance of Museum' do
     expect(@dmns).to be_instance_of(Museum)
@@ -44,7 +45,16 @@ RSpec.describe Museum do
       expect(@dmns.reccomended_exhibits(@patron_2)).to eq([@imax])
     end
   end
-  it 'initially has no patrons' do
-    expect(@dmns.patrons).to eq([])
-  end 
+  describe "Patrons" do
+    it 'initially has no patrons' do
+      expect(@dmns.patrons).to eq([])
+    end
+    it 'can admit patrons' do
+      @dmns.admit(@patron_1)
+      @dmns.admit(@patron_2)
+      @dmns.admit(@patron_3)
+      expect(@dmns.patrons).to eq([@patron_1, @patron_2, @patron_3])
+    end
+  end
+
 end
