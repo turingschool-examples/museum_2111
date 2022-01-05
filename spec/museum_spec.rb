83228bbc8b208015a8592052dcbf72b0546a16d9
oscar santos <oscar303data@gmail.com>
2022-01-05 09:26:56 -0700

    Feat: it exist block for museum is created

---
+++ spec/museum_spec.rb
@@ -0,0 +1,10 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Museum do
+  it 'exists' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns).to be_a(Museum)
+  end
+end
