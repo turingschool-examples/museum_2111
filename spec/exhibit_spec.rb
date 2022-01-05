7293ee73e9b555006ed14580cfdf472f5b60c33a
Joseph Galvin <jwgalvin@gmail.com>
2022-01-05 09:09:57 -0700

    Fix: exhibit spec typo

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -4,8 +4,8 @@ require 'pry'
 require 'Rspec'
 
 
-describe Patron do
-  describe "this will test the Patron class" do
+describe Exhibit do
+  describe "this will test the Exhibit class" do
     it "checks the functionality" do
       exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
       expect(exhibit.name).to eq("Gems and Minerals")
