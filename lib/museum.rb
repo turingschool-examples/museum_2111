5eeec0b1e5f0910301ea8ae4e15597d74db51d5c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 10:56:52 -0600

    Add #patrons_by_exhibit_interest to display hash of exhibits and patrons interested in each

--- lib/museum.rb
+++ lib/museum.rb
@@ -21,4 +21,18 @@ class Museum
     @patrons << patron
   end
 
+  def patrons_by_exhibit_interest
+    patrons_by_exhibit_interest = {}
+    @exhibits.each do |exhibit|
+      if !patrons_by_exhibit_interest.has_key?(exhibit)
+          patrons_by_exhibit_interest[exhibit] = []
+      end
+      @patrons.each do |patron|
+        if patron.interests.include?(exhibit.name)
+          patrons_by_exhibit_interest[exhibit] << patron
+        end
+      end
+    end
+    patrons_by_exhibit_interest
+  end
 end
