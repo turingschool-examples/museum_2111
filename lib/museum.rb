9c55a67449ac37e05e361155a1fd7913776cddae
Sam Smith <samlsmith424@gmail.com>
2022-01-05 09:41:36 -0800

    Feat: #patrons_by_exhibit_interest and lottery

--- lib/museum.rb
+++ lib/museum.rb
@@ -22,4 +22,31 @@ class Museum
   def admit(patron)
     @patrons.push(patron)
   end
+
+  def patron_interests(exhibit)
+    @patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def patrons_by_exhibit_interest
+    patrons_by_interest = Hash.new
+    @exhibits.each do |exhibit|
+      patrons_by_interest[exhibit] = patron_interests(exhibit)
+    end
+    patrons_by_interest
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    @patrons.find_all do |patron|
+      patron.spending_money < exhibit.cost
+    end
+  end
+
+  def draw_lottery_winner(exhibit)
+    names = ticket_lottery_contestants(exhibit).map do |ticket|
+      ticket.name
+    end
+    names.sample
+  end
 end
