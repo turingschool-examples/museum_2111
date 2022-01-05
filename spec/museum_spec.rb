8d678eafb8edc320f98480617186707cbfd710d7
Nick Cooley <cooley8706@gmail.com>
2022-01-05 09:47:59 -0700

    Test: pass patron attributes tests

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,11 +4,24 @@ require './lib/patron'
 RSpec.describe Exhibit do
   it 'exists' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_instance_of(Exhibit)
   end
 
   it 'has a name and cost' do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit.name).to eq("Gems and Minerals")
     expect(exhibit.cost).to be 0
+  end
+
+  it 'has a patron' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1).to be_instance_of(Patron)
+  end
+
+  it 'patron has money and interests' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to be 20
+    expect(patron_1.interests).to eq([])
   end
 end
