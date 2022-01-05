944753a273a9670e715fe379c6952ced012fa001
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-05 11:36:07 -0500

    feat: pass has a name and spending money tests

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,6 +1,8 @@
 class Patron
-  def initialize(argument)
-    @argument = argument
+  attr_reader :name, :spending_money
+  def initialize(name, spending_money)
+    @name = name
+    @spending_money = spending_money
   end
 
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe Patron do
     expect(patron_1).to be_a(Patron)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     patron_1 = Patron.new("Bob", 20)
     # binding.pry
     expect(patron_1.name).to eq("Bob")
   end
 
-  xit 'has a spending money' do
+  it 'has a spending money' do
     patron_1 = Patron.new("Bob", 20)
     # binding.pry
     expect(patron_1.spending_money).to eq(20)
