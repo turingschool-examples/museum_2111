8f16cfffa192835a890019645205e42e27d6c54b
Austin Moore <austinchristianmoore@gmail.com>
2022-01-05 11:11:57 -0500

    Feature: Iteration 1 tests

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,23 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  before (:each) do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it "exists" do
+    expect(exhibit).to be_instance_of(Exhibit)
+  end
+
+  it "has a name" do
+    exhibit.name
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it "has a cost" do
+    exhibit.cost
+    expect(exhibit.name).to eq(0)
+  end
+
+end
