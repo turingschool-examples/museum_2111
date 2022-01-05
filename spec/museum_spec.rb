0f080413b7890cc7650d2fc8910d460ccc6f72fe
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-05 09:42:44 -0700

    Add: it exists test for museum class

---
+++ spec/museum_spec.rb
@@ -0,0 +1,15 @@
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+RSpec.describe Museum do
+  before(:each) do
+    # @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    # @patron_1 = Patron.new("Bob", 20)
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  it 'exists' do
+    expect(@dmns).to be_instance_of Museum
+  end
+end
