2c2f886297eccd731d57cbd9efe4adecdea6085b
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:05:05 -0500

    Test: create exist test

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,23 @@
+require './lib/exhibit'
+RSpec.describe do
+  it 'exists' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit).to be_a(Exhibit)
+  end
+
+  it '' do
+  end
+  it '' do
+  end
+  it '' do
+  end
+  it '' do
+  end
+  it '' do
+  end
+  it '' do
+  end
+  it '' do
+  end
+end
