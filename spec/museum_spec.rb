7ed81a43da994a8419e8c2ef95b78df0b831c826
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-05 11:21:42 -0500

    Test: create it has exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe do
 
     expect(dmns.name).to eq("Denver Museum of Nature and Science")
   end
+
+  it 'has exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expect(dmns.exhibits).to eq([])
+  end
 end
