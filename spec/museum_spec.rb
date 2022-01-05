314a57d71597cb4b64f725c26aeb925b4c44aac3
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-05 11:30:22 -0500

    Add #can add exhibits test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,16 +5,23 @@ require './lib/museum'
 RSpec.describe Museum do
   subject { Museum.new("Denver Museum of Nature and Science") }
   before(:each) do
-    patron_1 = Patron.new("Bob", 20)
-    patron_2 = Patron.new("Sally", 20)
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
   context "iteration-2" do
     it "exists" do
       expect(subject.name).to eq("Denver Museum of Nature and Science")
       expect(subject.exhibits).to eq([])
+    end
+
+    it "can add exhibits" do
+      subject.add_exhibit(@gems_and_minerals)
+      subject.add_exhibit(@dead_sea_scrolls)
+      subject.add_exhibit(@imax)
+      expect(subject.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
     end
   end
 end
