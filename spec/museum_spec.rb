05c0dbbf0c5982c1507a80eb7a428f5588350115
Conor <>
2022-01-05 09:57:51 -0700

    Test: recommend_exhibit

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -30,4 +30,16 @@ RSpec.describe do
 
     expect(dmns.exhibits).to eq [gems_and_minerals, dead_sea_scrolls, imax]
   end
+
+  it "can recommend exhibits" do
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
 end
