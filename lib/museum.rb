bd88734fc67d8f59d1c3a3c9bf2e09cbdf343e87
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 10:38:40 -0600

    Add #admit to admit patrons into exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -4,6 +4,7 @@ class Museum
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -14,6 +15,10 @@ class Museum
     recommended = exhibits.select do |exhibit|
       exhibit if patron.interests.include?(exhibit.name)
     end
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 
 end
