386b521a44236ca536b5674bd865df5c3a443ce5
Jesse Ochs <jessescottochs@gmail.com>
2022-01-05 10:33:18 -0700

    Feat: reccomend exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -49,11 +49,15 @@ RSpec.describe Museum do
     expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 
-  xit 'can reccomend exhibits based on interests' do
+  it 'can reccomend exhibits based on interests' do
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
     patron_2.add_interest("IMAX")
-    expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
   end
 
