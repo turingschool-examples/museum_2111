c39c299fece37f60bf1d41890ea0bd185c76e978
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-05 09:08:55 -0700

    Add: test file and initial test for exhibit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,11 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it 'exists' do
+    expect(@exhibit).to be_instance_of Exhibit
+  end
