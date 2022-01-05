a7d342da6d2b48180b4126c0cb524fb6bbdc23b0
Conor <>
2022-01-05 09:28:59 -0700

    Test: add_interest

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -20,4 +20,11 @@ RSpec.describe do
   it "can has spending money" do
     expect(patron_1.spending_money).to eq 20
   end
+
+  it "can add interests" do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
+  end
 end
