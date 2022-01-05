1c3bd6c866479db50fefdfbc3594c7fe12885e34
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 10:14:47 -0600

    Test: Initialize Patron class with name, spending money, and no interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,6 +3,17 @@ require './lib/patron'
 RSpec.describe Patron do
 
   it 'exists' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1).to be_instance_of Patron
+  end
+
+  it 'initializes with a name, spending money, and no interests' do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
   end
 
 end
