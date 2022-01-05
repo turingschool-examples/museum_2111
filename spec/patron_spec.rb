900cd2711297c0dfa687b4ba63e2b6d97830b53d
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:17:49 -0700

    Test: #name, and #spending_money attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Patron do
   it 'will create an instance of patron' do
     expect(@patron_1).to be_instance_of(Patron)
   end
+  it 'will recognize name attribute' do
+    expect(@patron_1.name).to eq('Bob')
+  end
+  it "will recognize patron's spending money" do
+    expect(@patron_1.spending_money).to eq(20)
+  end
 
 end
