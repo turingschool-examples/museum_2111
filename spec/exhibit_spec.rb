23bac5cc7edfb376c8779e7c0c6b58426b8f9239
Jimmy Page <jpage@knights.ucf.edu>
2022-01-05 09:32:09 -0700

    removed before each block

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -3,15 +3,13 @@ require './lib/exhibit'
 require './lib/patron'
 
 describe Exhibit do
-  before :each do
-    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-  end
-
   it 'has a name' do
-    expect(@exhibit.name).to eq("Gems and Minerals")
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
   end
 
   it 'has a cost' do
-    expect(@exhibit.cost).to eq(0)
-  end 
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.cost).to eq(0)
+  end
 end
