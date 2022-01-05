57c96cc084e4f11fc0f979cf2c4e7dd1f4a72ceb
Jacob Jenner <jennerjacob@gmail.com>
2022-01-05 10:18:47 -0700

    Fix: test for recommend_exhibits, and wrote recommend_exhibits method(what a beast)

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,4 +11,16 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    @recommended_exhibits = []
+    patron.interests.each do |interest|
+      self.exhibits.each do |exhibit|
+        if exhibit.name == interest
+          @recommended_exhibits << exhibit
+        end
+      end
+    end
+    @recommended_exhibits
+  end
+  
 end
