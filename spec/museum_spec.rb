3f757bc1454fe3df3fd9ee96721f8d0b07242d6a
Jacob Jenner <jennerjacob@gmail.com>
2022-01-05 10:07:51 -0700

    Add: Wrote test for recommend_exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -25,7 +25,20 @@ RSpec.describe Museum do
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
+
     expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  it '#recommend_exhibits' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq(@gems_and_minerals, @dead_sea_scrolls)
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq(@imax)
   end
 
 end
