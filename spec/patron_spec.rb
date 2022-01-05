f73267cecea3fd5fd3dee9895ca2308196b2c27a
benistta <92293363+benistta@users.noreply.github.com>
2022-01-05 09:24:45 -0700

    Feat Added interests empty array to patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -17,4 +17,9 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.spending_money).to eq(20)
   end
+
+  it 'has interests is an empty array' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to eq([])
+  end
 end
