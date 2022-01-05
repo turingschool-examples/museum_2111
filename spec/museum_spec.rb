07e60917c764d103c30b58c0c3c0945b36adc1a2
Michael Brandt <msbrandt00@gmail.com>
2022-01-05 09:23:58 -0700

    Test: add test for existance of class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Museum do
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
   end
+  it 'will create an instance of Museum' do
+    expect(@dmns).to be_instance_of(Museum)
+  end 
 
 
 end
