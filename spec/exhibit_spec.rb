68ab3f0ddf74b46b8431fef57ceb5cd1013d9c63
Luke Pascale <lpluke5@gmail.com>
2022-01-05 09:31:52 -0700

    test - compelted first tests.

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,20 +1,20 @@
 require 'pry'
-require './lib/exhibit.rb'
+require './lib/exhibit'
+require './lib/patron'
 require 'rspec'
 
-describe Exhibit do
+RSpec.describe Exhibit do
+  describe 'iteration 1' do
+    it 'creates an instance of exhibit' do
+        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    # @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-  describe '#iteration 1' do
-    it 'creates an instance' do
-      @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        expect(exhibit).to be_an_instance_of(Exhibit)
+      end
 
-      expect(@exhibit).to be_an_instance_of(Exhibit)
-    end
+      it 'can have a name' do
+        exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    it 'can have a name' do
-
-      expect(exhibit.name).to eq("Gems and Minerals")
+        expect(exhibit.name).to eq("Gems and Minerals")
+      end
     end
   end
-end
