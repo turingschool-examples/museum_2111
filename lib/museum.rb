b1fb9a2262f00bd4738bd811de3c3339fb0389f5
Austin Moore <austinchristianmoore@gmail.com>
2022-01-05 11:27:48 -0500

    Feature: Begin Iteration 2

---
+++ lib/museum.rb
@@ -0,0 +1,8 @@
+class Museum
+  attr_reader
+
+  def initialize(argument)
+    @argument = argument
+  end
+
+end
---
+++ spec/museum_spec.rb
@@ -0,0 +1,7 @@
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+
+RSpec.describe Museum do
+end
