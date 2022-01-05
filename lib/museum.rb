d7933d7388e01ea1d60eb31aa940b82a6b4862df
Michael Hicks <hicks.michael04@gmail.com>
2022-01-05 10:30:05 -0700

    test: add can recommend exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,7 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patron
 
   def initialize(name)
     @name = name
@@ -8,7 +9,19 @@ class Museum
   end
 
   def add_exhibit(exhibit)
-    @exhibits << exhibit 
+    @exhibits << exhibit
+  end
 
+  def recommend_exhibits(patron)
+    recommended_exhibits = []
+    @exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        if exhibit.name == interest
+          recommended_exhibits << exhibit
+
+        end
+      end
+    end
+    recommended_exhibits.uniq
   end
 end
